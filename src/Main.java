5d5df11842553a4d0303a7ef06d1a4606aabed1d
Old Bread <noofnafnif@gmail.com>
2023-03-20 20:25:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 4868;
+        int bonus = price / 20;
+        System.out.println("Бонус за покупку билета составил " + bonus);
+
+    }
+}
